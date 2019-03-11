a5a7e52ebebfcc8b62c76c2232c7efcccf0f4530
MaximusNovak <maximusnovakmn@gmail.com>
2019-03-11 11:16:22 -0700

    added main menu

--- SOLVER/main.py
+++ SOLVER/main.py
@@ -1,3 +1,4 @@
+'''
 import sys
 from colorama import init
 init(strip=not sys.stdout.isatty()) #strip color is stdout is redirected
@@ -5,3 +6,34 @@ from termcolor import cprint
 from pyfiglet import figlet_format
 
 cprint(figlet_format('missile!', font='cybermedium'), 'green', 'on_grey', attrs=['bold'])
+'''
+
+import platform
+print("This solver runs on --> " + platform.python_version())
+
+def main_menu():
+    #Menu selection block
+    print("0: Rev_solver")
+    print("1: Pwn_solver")
+    print("2: Forensics_solver")
+    print("3: Crypto_solver")
+    print("4: Web")
+    print("5: Misc")
+
+    x = input("--> ")
+
+    if x == 0:
+        print("Test1")
+    elif x == 1:
+        print("Test2")    
+
+main_menu()
+
+'''
+     0: main_rev()
+     1: main_pwn()
+     2: main_forensics()
+     3: main_crypto()
+     4: main_web()
+     5: main_misc()
+'''
