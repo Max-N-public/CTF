922649a3b95043b3086f3399673b9db8d88a1e99
MaximusNovak <maximusnovakmn@gmail.com>
2019-03-12 09:27:52 -0700

    fixed some compiler issues

--- SOLVER/pwn/C_LIB/canary_collisions.c
+++ SOLVER/pwn/C_LIB/canary_collisions.c
@@ -3,7 +3,7 @@
 #define SIZE_32 4
 #define SIZE_64 8
 
-'''
+/*
 SUMMARY:
     A canary is a a reference to birds, miners used to bring with them into mines to signal when something went WRONG
     Some pwns require some sort of hash collision with the stack canary.
@@ -11,7 +11,7 @@ SUMMARY:
     Main techniques
         1) Canary relies on the rand() library which can be timing attacked
         2) Canary can be brute forced on 32 bit machines
-'''
+*/
 
 
 
@@ -19,5 +19,7 @@ int main()
 {
     srand(time(0));
 
-    for
+    //for
+
+    return 0;
 }
