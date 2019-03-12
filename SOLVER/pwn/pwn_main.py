c50c629e31056310c1673a04b48d62affdf459f6
Max Novak <maximusnovakmn@gmail.com>
2019-03-12 04:04:31 -0700

    began diagram of stack in pwn

---
+++ SOLVER/pwn/pwn_main.py
@@ -0,0 +1,54 @@
+from subprocess import call
+
+#/bin/sh shellcode strings
+bin_sh_25 = "\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x31\xc0\x99\x31\xf6\x54\x5f\xb0\x3b\x0f\x05"
+
+'''
+0xff
+|++++++++++++++|
+|   \t         |
+|   arg3       | <--- 0x00ff332200
+|++++++++++++++| 
+|   eip        | <--- 0x00ff332200+4
+|++++++++++++++|
+|   ebp        | <--- 0x00ff332200+4
+|
+0x00
+'''
+
+#run enumeration scripts on a pwn and output to screen
+#add learning if/else struct to determine what the problem type is
+def pwn_enumeration(filename):
+    call('checksec filename')
+
+released = {
+		"iphone" : 2007,
+		"iphone 3G" : 2008,
+		"iphone 3GS" : 2009,
+		"iphone 4" : 2010,
+		"iphone 4S" : 2011,
+		"iphone 5" : 2012
+	}
+#test dictionary
+testdict = {
+        "arg3":"0x00ff332200",
+        "EIP":"0x00ff332204",
+        "ebp":"0x00ff332208",
+        "trashend":"0x00ff33220B",
+        "trashstart":"0x00ff332200",
+        "int":"0x00ff332200"
+    }
+
+#YO WTF DICTIONARIES DONT OUTPUT IN CORRECT ORDER
+def print_output(address_list):
+    print("STACK")
+    print("|" + "+"*27 + "|")
+    for name,address in address_list.items():
+        print("|" + " "*27 + "|")
+        print("|\t" + name + " "*(20-len(name)) + "|" + "<---" + address)
+        print("|" + " "*27 + "|")
+        print("|" + "+"*27 + "|")
+
+print_output(testdict)
+
+        
