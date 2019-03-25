7174496dc280e39e9137443d6a19f04f1f921ff7
Max Novak <maximusnovakmn@gmail.com>
2019-03-24 21:04:35 -0700

    learning flask and update read me

--- SOLVER/crypto/cipher_bruteforce/cipher_lib.py
+++ SOLVER/crypto/cipher_bruteforce/cipher_lib.py
@@ -1,3 +1,19 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def cipher_lib():
+    return "hello world"
+
+@app.route('/call_decoders')
+def main():
+    return 'text'
+
+'''def base64_decode(input):
+{
+    return "test string"
+}
+
 def rot47(input)
 {
 
@@ -12,3 +28,4 @@ def runCaesar(input)
 {
 
 }
+'''
