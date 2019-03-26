5cc6fa2a6cb72b87638833b911d8df655a97bda3
MaximusNovak <maximusnovakmn@gmail.com>
2019-03-26 11:38:10 -0700

    added rot block

--- SOLVER/RUNME.sh
+++ SOLVER/RUNME.sh
@@ -6,11 +6,15 @@ pip install colorama
 #shell interaction
 pip install pexpect
 
+#pwn
 apt-get install readelf
 apt-get install gdb-doc
 
+#Data type conversions
+pip install hackercodecs
 
 #download ghidra
 wget "ghidra_9.0_PUBLIC_20190228.zip" -O /pwn/Programs/
 
 #update github script repos
+
--- SOLVER/crypto/cipher_bruteforce/routes.py
+++ SOLVER/crypto/cipher_bruteforce/routes.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-'''
-cipher imports
-'''
 import base64
 
 
@@ -11,24 +8,20 @@ import base64
 @app.route('/ciphers', methods=['GET'])
 def cipher_lib(input):
     '''
-    Call list for ciphers:
-    base64
+    Call list for ciphers: (Only has really common ciphers --> use cyberchef for more rare)
+    base64/32/16
     ROT47
     ROT13
     Caeser Cipher
-    Affine
-    Atbash
     Morse
     QuipQuip
     '''
-    '''
 
+    '''
     base64_decoded = base64(input)
     rot47_decoded = rot47(input)
     rot13_decoded = rot13(input)
     caesar_decoded = caesar(input)
-    affine_decoded = affine(input)
-    atbash_decoded = atbash(input)
     morse_decoded = morse(input)
     quipquip_decoded = quipquip(input)
     '''
@@ -57,8 +50,17 @@ def base32_encode(input):
 def base16_encode(input):
     return base64.b16encode(input)
 
+#--------------------------------------------
+
+'''
+ROT Block-------------------------------------
+'''
 def rot47(input):
     return "test"
+
+def rot13(input)
+
+#---------------------------------------------
 
 def runRot(input):
     return "aasd"
