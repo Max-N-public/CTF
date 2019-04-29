19f4a3d2d68b85775c101ce8296ec4ed5283290f
Max Novak <maximusnovakmn@gmail.com>
2019-04-29 02:15:15 -0700

    format string notes

---
+++ SOLVER/PUT_ALL_HERE_ORGANIZE_LATER/format_string_tester.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int banana;
+    int secret = 0;
+    printf(banana);
+
+    return 0;
+}
---
+++ SOLVER/PUT_ALL_HERE_ORGANIZE_LATER/python_tester.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import string
+a = search_space = '{}' + string.uppercase + string.lowercase + string.digits + '-_'
+#x = int()[0]
+print a
